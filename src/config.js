12306c03abb304d4caa90a78391afc02a90fb635
kychitoge <jay2006tt@gmail.com>
2025-12-26 14:58:49 +0700

    Fix: Them http vao file config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // File này chứa IP thật
 const CONFIG = {
-    HOST: "192.168.0.101:9999", // IP thật của ông
+    HOST: "http://192.168.0.101:9999", // IP thật của ông
     API_KEY: ""      // (Tùy chọn) Mã bảo mật nếu ông có thiết lập
 };
